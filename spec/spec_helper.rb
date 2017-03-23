3d931d92518e7a1ce45817c2a82c3a66ca4b8734
Garrett Thornburg <film42@gmail.com>
2017-03-23 16:00:27 -0400

    Add a few defaults to rspec config for order and color

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require "fake_nats_client"
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
+  config.order = :random
+  config.color = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
