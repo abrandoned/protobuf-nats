64165e967c13e602e78873a8901921cfc60c1be4
Garrett Thornburg <film42@gmail.com>
2017-03-23 12:13:40 -0400

    Remove duplicate code from bench scripts and set PB env vars

--- bench/client.rb
+++ bench/client.rb
@@ -1,34 +1,26 @@
-require "nats/io/client"
-require "thread"
-require "benchmark/ips"
-require "protobuf"
-require 'protobuf/message'
-require 'protobuf/rpc/service'
+ENV["PB_CLIENT_TYPE"] = "protobuf/nats/client"
+ENV["PB_SERVER_TYPE"] = "protobuf/nats/runner"
 
-module Warehouse
-  class Shipment < ::Protobuf::Message
-    optional :string, :guid, 1
-    optional :string, :address, 2
-    optional :double, :price, 3
-    optional :string, :package_guid, 4
-  end
-end
+require "benchmark/ips"
+require "./examples/warehouse/app"
 
 payload = ::Warehouse::Shipment.new(:guid => ::SecureRandom.uuid, :address => "123 yolo st")
-$request = ::Protobuf::Socketrpc::Request.new(:service_name => "Warehouse",
-                                               :method_name => "create",
-                                               :request_proto => payload.encode).encode
+request = ::Protobuf::Socketrpc::Request.new(:service_name => "::Warehouse::ShipmentService",
+                                             :method_name => "create",
+                                             :request_proto => payload.encode).encode
 
-$nats = ::NATS::IO::Client.new
-$nats.connect({:servers => ["nats://127.0.0.1:4222"]})
+nats = ::NATS::IO::Client.new
+nats.connect({:servers => ["nats://127.0.0.1:4222"]})
 
-subscription_key_and_queue = "Warehouse::ShipmentService::create"
+subscription_key_and_queue = "rpc.warehouse.shipment_service.create"
+
+Protobuf::Logging.logger = ::Logger.new(nil)
 
 Benchmark.ips do |config|
   config.warmup = 10
   config.time = 10
 
   config.report("single threaded performance") do
-    $nats.request(subscription_key_and_queue, $request)
+    nats.request(subscription_key_and_queue, request)
   end
 end
