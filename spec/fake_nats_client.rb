cc516fdf9a6c8a93461b6d8dee7863549066080c
Garrett Thornburg <film42@gmail.com>
2017-04-05 13:34:57 -0400

    Add next message to fake client and fix client tests

--- spec/fake_nats_client.rb
+++ spec/fake_nats_client.rb
@@ -31,6 +31,14 @@ class FakeNatsClient
   def unsubscribe(*)
   end
 
+  def next_message(_sub, timeout)
+    timeout = timeout / 1_000.to_f
+    started_at = ::Time.now
+    @next_message = nil
+    sleep 0.01 while @next_message.nil? && timeout > (::Time.now - started_at)
+    @next_message
+  end
+
   def schedule_message(message)
     schedule_messages([message])
   end
@@ -42,6 +50,7 @@ class FakeNatsClient
           sleep message.seconds_in_future
           block = subscriptions[message.subject]
           block.call(message.data) if block
+          @next_message = message
         rescue => error
           puts error
         end
