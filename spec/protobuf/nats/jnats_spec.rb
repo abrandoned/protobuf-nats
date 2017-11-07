4ec7fd787b6653bf95ed0ab752175e7f035e3c30
Garrett Thornburg <film42@gmail.com>
2017-11-07 15:55:14 -0500

    Allow an auto-connect to happen if the @connection is nil

---
+++ spec/protobuf/nats/jnats_spec.rb
@@ -0,0 +1,24 @@
+require "rspec"
+
+if defined?(JRUBY_VERSION)
+  require "protobuf/nats/jnats"
+
+  describe ::Protobuf::Nats::JNats do
+    describe "#connection" do
+      it "calls #connect when no @connection exists" do
+        expect(subject).to receive(:connect).with({})
+        subject.connection
+      end
+
+      it "attempts to reconnect with options given to #connect" do
+        allow(::Java::IoNatsClient::ConnectionFactory).to receive(:new).and_raise(::RuntimeError)
+        provided_options = {:yolo => "ok"}
+        subject.connect(provided_options) rescue nil
+        expect(subject.options).to eq(provided_options)
+
+        expect(subject).to receive(:connect).with(provided_options)
+        subject.connection rescue nil
+      end
+    end
+  end
+end
