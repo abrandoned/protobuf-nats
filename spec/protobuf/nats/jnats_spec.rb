dd45a3131f732ec3b5c1b454f3a3cca78e529252
Garrett Thornburg <film42@gmail.com>
2020-08-07 16:41:56 -0700

    Fixes newer jruby errors by patching java-nats
    Changes are stored here: https://github.com/film42/java-nats/tree/jruby-compat

--- spec/protobuf/nats/jnats_spec.rb
+++ spec/protobuf/nats/jnats_spec.rb
@@ -20,5 +20,70 @@ if defined?(JRUBY_VERSION)
         subject.connection rescue nil
       end
     end
+
+    describe "#connect" do
+      it "creates a new message channel" do
+        subject.connect
+        subject.close
+      end
+    end
+
+    context "integration tests" do
+      context "async subscribe" do
+        before { subject.connect }
+        after { subject.close rescue nil }
+
+        it "can subscribe async and receive a message" do
+          # Set up an async receiver.
+          server_sub = subject.subscribe("yolo.brolo", :queue => "yolo.brolo") do |request, reply_id, _subject|
+            expect(request).to eq("hello")
+            subject.publish(reply_id, "received")
+            subject.flush
+          end
+
+          # Set up a blocking subscription for the reply.
+          client_sub = subject.subscribe("hit.me.back")
+
+          # Send a message to the server.
+          subject.publish("yolo.brolo", "hello", "hit.me.back")
+          subject.flush
+
+          # Use client to wait for response.
+          response = subject.next_message(client_sub, 1)
+          expect(response.data).to eq("received")
+
+          # Clean up
+          subject.unsubscribe(client_sub)
+          subject.unsubscribe(server_sub)
+        end
+      end
+    end
+
+    context "auto unsubscribe" do
+      before { subject.connect }
+      after { subject.close rescue nil }
+
+      it "can auto unsub after n messages" do
+        sub = subject.subscribe("hey.dude", :max => 2)
+
+        expect(sub.isClosed).to eq(false)
+
+        # First message
+        subject.publish("hey.dude", "message1")
+        response = subject.next_message(sub, 1)
+        expect(response.data).to eq("message1")
+
+        # Second message
+        subject.publish("hey.dude", "message2")
+        response = subject.next_message(sub, 1)
+        expect(response.data).to eq("message2")
+
+        # All done
+        # response = subject.next_message(sub, 1)
+        # puts(response)
+        # subject.unsubscribe(sub)
+        expect(sub.isClosed).to eq(true)
+      end
+    end
   end
 end
